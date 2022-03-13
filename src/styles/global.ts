dbacbd61f1649c13c755e7a5ffb47f67593c1977
Nikolas.Dev <nikkolas21082003@gmail.com>
2022-03-13 00:58:28 -0300

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import Background from "../assets/background.png";
 
 export const GlobalStyle = createGlobalStyle`
-
   :root {
     --body-background: #0e0e0e;
     --color-logo: #ff5b50;
